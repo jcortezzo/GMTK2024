e9999ddfc20eaa41d6c75ff36fe7cb3880d47e27
Nguyen Duong <ducnguyenduchao@gmail.com>
2024-08-18 01:17:29 -0700

    Basic level up logic

--- GMTK2024/Assets/Scripts/Player/PlayerStat.cs
+++ GMTK2024/Assets/Scripts/Player/PlayerStat.cs
@@ -8,10 +8,11 @@ public class PlayerStat : MonoBehaviour
     public int Health { get; private set; }
     [field: SerializeField]
     public int GroundEaten { get; private set; }
+    [field: SerializeField]
     public int Level { get; private set; }
     private void Start()
     {
-        Level = 1;
+        Level = 0;
     }
     public void Damage(int amount)
     {
@@ -26,14 +27,23 @@ public class PlayerStat : MonoBehaviour
 
     private void Update()
     {
-        
+        var nextLevelGroundCount = GetNumberOfGroundForNextLevel();
+        if(GroundEaten >= nextLevelGroundCount)
+        {
+            LevelUp();
+        }
     }
 
+    private void LevelUp()
+    {
+        Level++;
+        transform.localScale = new Vector3(Level, Level, Level);
+    }
     private int GetNumberOfGroundForNextLevel()
     {
         // y = mx + b
         int b = 20;
-        int m = 2;
+        int m = 10;
         return m * Level + b;
     }
 }
