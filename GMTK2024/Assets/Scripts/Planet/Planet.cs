a9266c3ae02184e8db8445c1ce29ab23fae22dfb
Jonah Cortezzo <jonahvincent@gmail.com>
2024-08-18 18:44:45 -0700

    add initial particle system

--- GMTK2024/Assets/Scripts/Planet/Planet.cs
+++ GMTK2024/Assets/Scripts/Planet/Planet.cs
@@ -1,17 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    private CircleCollider2D cc2d;
-    private PlanetGenerator planetGenerator;
+    private CircleCollider2D _cc2d;
+    private PlanetGenerator _planetGenerator;
+    private ParticleSystem _particleSystem;
+
+    private static Color[] _colors = new Color[]
+    {
+        HexToColor("362023"),
+        HexToColor("700548"),
+        HexToColor("7272ab"),
+        HexToColor("7899d4")
+    };
 
     // Start is called before the first frame update
     void Start()
     {
-        cc2d = GetComponent<CircleCollider2D>();
-        planetGenerator = GetComponent<PlanetGenerator>();
-        cc2d.radius = planetGenerator.Radius * 2;
+        _cc2d = GetComponent<CircleCollider2D>();
+        _planetGenerator = GetComponent<PlanetGenerator>();
+        _particleSystem = GetComponent<ParticleSystem>();
+
+        _cc2d.radius = _planetGenerator.Radius * 2;
+
+        InitParticleSystem();
+        // main.startColor = new ParticleSystem.MinM
+    }
+
+    private void InitParticleSystem()
+    {
+        var shape = _particleSystem.shape;
+        shape.shapeType = ParticleSystemShapeType.Circle;
+        shape.radius = _cc2d.radius;
+
+        var main = _particleSystem.main;
+        main.startColor = _colors.OrderBy(c => Random.value).First();
+    }
+
+    static Color HexToColor(string hex)
+    {
+        if (ColorUtility.TryParseHtmlString("#" + hex, out Color color))
+        {
+            return color;
+        }
+        Debug.LogError("Invalid hex color: " + hex);
+        return Color.white;
     }
 }
